b7c3317a4731b20b18a449d59bd0c10c3ad79bfd
Muqsit <hackhack05@gmail.com>
2024-11-02 10:29:20 +0000

    poggit: do not type-hint constants

--- src/muqsit/worldactionlog/DefaultActions.php
+++ src/muqsit/worldactionlog/DefaultActions.php
@@ -6,12 +6,12 @@ namespace muqsit\worldactionlog;
 
 final class DefaultActions{
 
-	public const string NAMESPACE = "wal:";
+	public const NAMESPACE = "wal:";
 
-	public const string BLOCK_ENTITY_BREAK = self::NAMESPACE . "block_entity_break";
-	public const string BLOCK_ENTITY_INTERACT = self::NAMESPACE . "block_entity_interact";
-	public const string BLOCK_ENTITY_PLACE = self::NAMESPACE . "block_entity_place";
-	public const string CHUNK_ENTER = self::NAMESPACE . "chunk_enter";
-	public const string CHUNK_EXIT = self::NAMESPACE . "chunk_exit";
-	public const string INVENTORY_OPEN = self::NAMESPACE . "inventory_open";
+	public const BLOCK_ENTITY_BREAK = self::NAMESPACE . "block_entity_break";
+	public const BLOCK_ENTITY_INTERACT = self::NAMESPACE . "block_entity_interact";
+	public const BLOCK_ENTITY_PLACE = self::NAMESPACE . "block_entity_place";
+	public const CHUNK_ENTER = self::NAMESPACE . "chunk_enter";
+	public const CHUNK_EXIT = self::NAMESPACE . "chunk_exit";
+	public const INVENTORY_OPEN = self::NAMESPACE . "inventory_open";
 }
